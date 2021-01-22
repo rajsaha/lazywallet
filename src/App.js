76455a329f4120513498d4fde2329698637bd750
Raj Saha <saha.raj@mobileone.com.my>
2021-01-22 12:09:09 +0800

    fixed nav

--- src/App.js
+++ src/App.js
@@ -1,66 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import { Home, History, AttachMoney, Person } from '@material-ui/icons';
 import Homepage from '@Pages/Home/Home';
 import {
-  BrowserRouter as Router, Route, Switch, useHistory
+  BrowserRouter as Router
 } from "react-router-dom";
+import { Route } from "react-router";
 import Regulars from '@Pages/Regulars/Regulars';
-
-const useStyles = makeStyles({
-  root: {
-    width: "100%",
-    position: "fixed",
-    bottom: 0,
-    left: 0,
-    'z-index': 1000,
-  },
-});
+import PrimaryNav from '@Components/PrimaryNav/PrimaryNav';
 
 function App() {
-  const classes = useStyles();
-  const [value, setValue] = useState(0);
-  let history = useHistory();
-
   return (
-    <div className="content-container">
-      <div className="content">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route exact path="/regulars" component={Regulars} />
-          </Switch>
-        </Router>
+    <Router>
+      <div className="content-container">
+        <div className="content">
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route exact path="/regulars">
+            <Regulars />
+          </Route>
+        </div>
       </div>
-      <BottomNavigation
-        value={value}
-        onChange={(event, newValue) => {
-          console.log(value);
-          setValue(newValue);
-          switch (value) {
-            case 1:
-              history.push('/');
-              break;
-            case 0:
-              history.push('/regulars');
-              break;
-            default:
-              history.push('/');
-              break;
-          }
-        }}
-        showLabels
-        className={classes.root}
-      >
-        <BottomNavigationAction label="Home" icon={<Home />} />
-        <BottomNavigationAction label="Regulars" icon={<AttachMoney />} />
-        <BottomNavigationAction label="History" icon={<History />} />
-        <BottomNavigationAction label="Account" icon={<Person />} />
-      </BottomNavigation>
-    </div>
+      <PrimaryNav />
+    </Router>
   );
 }
 
---
+++ src/components/PrimaryNav/PrimaryNav.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { withRouter } from "react-router";
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import { Home, History, AttachMoney, Person } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+
+function PrimaryNav() {
+    const state = {
+        value: 0,
+        pathMap: [
+            '/',
+            '/regulars',
+            '/',
+            '/',
+        ]
+    };
+    const useStyles = makeStyles({
+        root: {
+            width: "100%",
+            position: "fixed",
+            bottom: 0,
+            left: 0,
+            'z-index': 1000,
+            "& .MuiBottomNavigationAction-root": {
+                "@media (max-width: 768px)": {
+                    minWidth: "auto",
+                    padding: "6px 0"
+                }
+            }
+        },
+    });
+    const classes = useStyles();
+    const [btmNavState, setState] = useState(0);
+    const handleChange = (event, value) => {
+        setState(value);
+    };
+
+
+    return (
+        <>
+            <BottomNavigation
+                value={btmNavState}
+                onChange={handleChange}
+                showLabels
+                className={classes.root}
+            >
+                <BottomNavigationAction label="Home" icon={<Home />} component={Link} to={state.pathMap[0]} />
+                <BottomNavigationAction label="Regulars" icon={<AttachMoney />} component={Link} to={state.pathMap[1]} />
+                <BottomNavigationAction label="History" icon={<History />} />
+                <BottomNavigationAction label="Account" icon={<Person />} />
+            </BottomNavigation>
+        </>
+    );
+}
+
+export default withRouter(PrimaryNav);
