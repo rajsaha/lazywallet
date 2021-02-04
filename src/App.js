bcf307d12274ca6b8e1088ef3c8a07fda19ec623
Raj Saha <saha.raj@mobileone.com.my>
2021-02-04 15:11:14 +0800

    wip dark mode

--- src/App.js
+++ src/App.js
@@ -4,29 +4,43 @@ import Homepage from '@Pages/Home/Home';
 import {
     BrowserRouter as Router
 } from "react-router-dom";
-import {Route} from "react-router";
+import { Route } from "react-router";
 import Regulars from '@Pages/Regulars/Regulars';
 import PrimaryNav from '@Components/PrimaryNav/PrimaryNav';
 import History from '@Pages/History/History';
+import { createMuiTheme, ThemeProvider, useMediaQuery } from '@material-ui/core';
 
 function App() {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+    const theme = React.useMemo(
+        () =>
+            createMuiTheme({
+                palette: {
+                    type: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
+
     return (
-        <Router>
-            <div className="app">
-                <div className="content-container">
-                    <Route exact path="/">
-                        <Homepage/>
-                    </Route>
-                    <Route exact path="/regulars">
-                        <Regulars/>
-                    </Route>
-                    <Route exact path="/history">
-                        <History/>
-                    </Route>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <div className="app">
+                    <div className="content-container">
+                        <Route exact path="/">
+                            <Homepage />
+                        </Route>
+                        <Route exact path="/regulars">
+                            <Regulars />
+                        </Route>
+                        <Route exact path="/history">
+                            <History />
+                        </Route>
+                    </div>
+                    <PrimaryNav />
                 </div>
-                <PrimaryNav/>
-            </div>
-        </Router>
+            </Router>
+        </ThemeProvider>
     );
 }
 
